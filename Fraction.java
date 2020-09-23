924812d950089082013b8502a81f3269c39fcbc8
uvsq21801870 <amy.blondeau@ens.uvsq.fr>
2020-09-23 18:14:37 +0200

    envoie

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,14 @@
 
 public class Fraction {
+	
+	public int nominateur;
+	public int denominateur;
+	
+	
+	public String toString() {
+		int f;
+		f = this.denominateur/this.nominateur;
+		return "La fraction est " + f;
+	}
 
 }
--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Fraction  f = new Fraction();
+		f.denominateur = 8;
+		f.nominateur = 2;
+		System.out.println(f);
 	}
 
 }
